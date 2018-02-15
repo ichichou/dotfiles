8991633a67a0a7cd089c91509ae4d9ddaaaa6786
ichichou <flower.sun.rain.silver@gmail.com>
2018-02-15 21:34:39 +0900

    体裁を微修正

--- config.fish
+++ config.fish
@@ -1,5 +1,4 @@
-# Aliases
-
+### Aliases
 alias rm 'rmtrash'
 
 alias mv 'mv -i'
@@ -10,13 +9,12 @@ alias ll 'ls -lG'
 alias lla 'ls -alG'
 
 
-# Functions
-
+### Functions
 function fish_user_key_bindings
-  bind \cr 'peco_select_history (commandline -b)'
+    bind \cr 'peco_select_history (commandline -b)'
 end
 
 
-# Greeting Message
-
+### Greeting Message
 set fish_greeting
+
