ab151d19c2332c8f2df29f077cd428cc400877a1
ichichou <flower.sun.rain.silver@gmail.com>
2018-03-05 00:57:21 +0900

    Remove ls 関連のエイリアス
    la, ll は fish 標準のエイリアスとして用意されていたため。
    la は ls -alG のエイリアスである。

--- config.fish
+++ config.fish
@@ -1,20 +1,19 @@
 ### Aliases
+
 alias rm 'rmtrash'
 
 alias mv 'mv -i'
 alias cp 'cp -i'
 
-alias la 'ls -aG'
-alias ll 'ls -lG'
-alias lla 'ls -alG'
-
 
 ### Functions
+
 function fish_user_key_bindings
     bind \cr 'peco_select_history (commandline -b)'
 end
 
 
 ### Greeting Message
+
 set fish_greeting
 
