fb0bb48e1be72d3397b5f0d92a9d7924865efe55
ichichou <flower.sun.rain.silver@gmail.com>
2018-12-18 01:07:40 +0900

    first commit

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -u
+
+DOT_DIR="${HOME}/dotfiles"
+CONFIG_DIR="${HOME}/.config"
+
+for f in .??*
+do
+    [ "$f" == ".git" ] && continue
+    [ "$f" == ".DS_Store" ] && continue
+
+    ln -snfv ${DOT_DIR}/${f} ${HOME}/${f}
+done
+
+ln -snfv ${DOT_DIR}/config.fish ${CONFIG_DIR}/fish/config.fish
+ln -snfv ${DOT_DIR}/karabiner.json ${CONFIG_DIR}/karabiner/karabiner.json
+
