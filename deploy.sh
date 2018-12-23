543ac253bcf4d1fcf584e0bd65e6b6669cdb3276
ichichou <flower.sun.rain.silver@gmail.com>
2018-12-23 20:00:16 +0900

    記法を細かく変更。

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-set -u
+set -eu
 
-DOT_DIR="${HOME}/dotfiles"
-CONFIG_DIR="${HOME}/.config"
+DOT_DIR="$HOME/dotfiles"
+CONFIG_DIR="$HOME/.config"
 
+# dotfiles を復元する手段
+
+cd $DOT_DIR
 for f in .??*
 do
-    [ "$f" == ".git" ] && continue
-    [ "$f" == ".gitignore" ] && continue
-    [ "$f" == ".DS_Store" ] && continue
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitignore" ] && continue
+    [ "$f" = ".DS_Store" ] && continue
 
-    ln -snfv ${DOT_DIR}/${f} ${HOME}/${f}
+    ln -snfv $DOT_DIR/$f $HOME/$f
 done
 
-ln -snfv ${DOT_DIR}/config.fish ${CONFIG_DIR}/fish/config.fish
-ln -snfv ${DOT_DIR}/karabiner.json ${CONFIG_DIR}/karabiner/karabiner.json
+mkdir -p $CONFIG_DIR
+ln -snfv $DOT_DIR/config.fish $CONFIG_DIR/fish/config.fish
+ln -snfv $DOT_DIR/karabiner.json $CONFIG_DIR/karabiner/karabiner.json
 
