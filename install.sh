922b0ce129646ed915188fb7ea3988a13d4e39f1
ichichou <flower.sun.rain.silver@gmail.com>
2018-12-24 12:59:44 +0900

    dotfiles/ がないときに dotfiles リポジトリを git clone

--- install.sh
+++ install.sh
@@ -2,22 +2,24 @@
 
 set -eu
 
-DOT_DIR="$HOME/dotfiles"
-CONFIG_DIR="$HOME/.config"
+DOT_DIR="${HOME}/dotfiles"
+CONFIG_DIR="${HOME}/.config"
 
-# dotfiles を復元する手段
+if [[ ! -e ${DOT_DIR} ]]; then
+    mkdir -p ${DOT_DIR}
+    cd ${HOME}
+    git clone https://github.com/ichichou/dotfiles.git
+fi
 
-cd $DOT_DIR
-for f in .??*
-do
-    [ "$f" = ".git" ] && continue
-    [ "$f" = ".gitignore" ] && continue
-    [ "$f" = ".DS_Store" ] && continue
-
-    ln -snfv $DOT_DIR/$f $HOME/$f
+cd ${DOT_DIR}
+for f in .??*; do
+    [[ ${f} == ".git" ]] && continue
+    [[ ${f} == ".gitignore" ]] && continue
+    [[ ${f} == ".DS_Store" ]] && continue
+    ln -snfv ${DOT_DIR}/${f} ${HOME}/${f}
 done
 
-mkdir -p $CONFIG_DIR
-ln -snfv $DOT_DIR/config.fish $CONFIG_DIR/fish/config.fish
-ln -snfv $DOT_DIR/karabiner.json $CONFIG_DIR/karabiner/karabiner.json
+mkdir -p ${CONFIG_DIR}/fish ${CONFIG_DIR}/karabiner
+ln -snfv ${DOT_DIR}/config.fish ${CONFIG_DIR}/fish/config.fish
+ln -snfv ${DOT_DIR}/karabiner.json ${CONFIG_DIR}/karabiner/karabiner.json
 
