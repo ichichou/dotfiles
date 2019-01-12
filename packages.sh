731d06e61f0c1eedba3247f40db08d4a8ca257f3
ichichou <35998815+ichichou@users.noreply.github.com>
2019-01-12 16:00:04 +0900

    プラグインリスト追加・削除

--- packages.sh
+++ packages.sh
@@ -4,21 +4,20 @@ set -eu
 
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 fisher self-update
-fisher add alecgorge/fish-flash-theme
 fisher add jethrokuan/fzf
 fisher add jethrokuan/z
-fisher add jorgebucaran/fish-mono
-fisher add jorgebucaran/fish-sol
+fisher add jorgebucaran/fish-spin
 fisher add rafaelrinaldi/pure
 
 gem update --system
 gem install asciidoctor
-gem install asciidoctor-diagram
 gem install asciidoctor-pdf --pre
 gem install asciidoctor-pdf-cjk
+gem install asciidoctor-diagram
 
 npm update -g npm
 npm install -g gitbook-cli
 npm install -g mermaid
 npm install -g mermaid.cli
+npm install -g instant-markdown-d
 
