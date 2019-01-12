54eaa601776e59f54b299d22826b2c4121e6af7a
ichichou <35998815+ichichou@users.noreply.github.com>
2019-01-12 15:47:36 +0900

    デフォルト設定をコメントアウト

--- .hyper.js
+++ .hyper.js
@@ -1,84 +1,77 @@
-// Future versions of Hyper may add additional config options,
-// which will not automatically be merged into this file.
-// See https://hyper.is#cfg for all currently supported options.
-
 module.exports = {
   config: {
-    updateChannel: 'stable',
+    // updateChannel: 'stable',
     fontSize: 12,
-    fontFamily: 'Hack, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
-    fontWeight: 'normal',
-    fontWeightBold: 'bold',
-    lineHeight: 1.1,
-    letterSpacing: 0,
-    cursorColor: 'rgba(248,28,229,0.8)',
-    cursorAccentColor: '#000',
-    cursorShape: 'BLOCK',
-    cursorBlink: false,
-    foregroundColor: '#fff',
-    backgroundColor: '#000',
-    selectionColor: 'rgba(248,28,229,0.3)',
-    borderColor: '#333',
-    css: '',
-    termCSS: '',
-    showHamburgerMenu: '',
-    showWindowControls: '',
-    padding: '12px 14px',
+    fontFamily: 'Hack',
+    // fontWeight: 'normal',
+    // fontWeightBold: 'bold',
+    lineHeight: 1.2,
+    // letterSpacing: 0,
+    // cursorColor: 'rgba(248,28,229,0.8)',
+    // cursorAccentColor: '#000',
+    // cursorShape: 'BLOCK',
+    // cursorBlink: false,
+    // foregroundColor: '#fff',
+    // backgroundColor: '#000',
+    // selectionColor: 'rgba(248,28,229,0.3)',
+    // borderColor: '#333',
+    // css: '',
+    // termCSS: '',
+    // showHamburgerMenu: '',
+    // showWindowControls: '',
+    // padding: '12px 14px',
 
-    colors: {
-      black: '#000000',
-      red: '#C51E14',
-      green: '#1DC121',
-      yellow: '#C7C329',
-      blue: '#0A2FC4',
-      magenta: '#C839C5',
-      cyan: '#20C5C6',
-      white: '#C7C7C7',
-      lightBlack: '#686868',
-      lightRed: '#FD6F6B',
-      lightGreen: '#67F86F',
-      lightYellow: '#FFFA72',
-      lightBlue: '#6A76FB',
-      lightMagenta: '#FD7CFC',
-      lightCyan: '#68FDFE',
-      lightWhite: '#FFFFFF',
-    },
-
-    shell: '',
-    shellArgs: ['--login'],
-    env: {},
-    bell: 'false',
-    copyOnSelect: false,
-    defaultSSHApp: true,
-
-    // base16: {
-    //   scheme: 'tomorrow-night'
-    //   base00: '#000000'
+    // colors: {
+    //   black: '#000000',
+    //   red: '#C51E14',
+    //   green: '#1DC121',
+    //   yellow: '#C7C329',
+    //   blue: '#0A2FC4',
+    //   magenta: '#C839C5',
+    //   cyan: '#20C5C6',
+    //   white: '#C7C7C7',
+    //   lightBlack: '#686868',
+    //   lightRed: '#FD6F6B',
+    //   lightGreen: '#67F86F',
+    //   lightYellow: '#FFFA72',
+    //   lightBlue: '#6A76FB',
+    //   lightMagenta: '#FD7CFC',
+    //   lightCyan: '#68FDFE',
+    //   lightWhite: '#FFFFFF',
     // },
 
+    // shell: '',
+    // shellArgs: [ '--login' ],
+    // env: {},
+    // bell: 'false',
+    // copyOnSelect: false,
+    // defaultSSHApp: true,
+
+    base16: { scheme: 'tomorrow-night' },
+
     // MaterialTheme: {
-    //   theme: '', // OPTIONS: 'Darker', 'Palenight', 'Ocean', ''
-    //   backgroundOpacity: '0.7', // OPTIONS: From 0.1 to 1
+    //   theme: '',
+    //   backgroundOpacity: '0.7',
     //   accentColor: '#64FFDA',
-    //   vibrancy: 'dark' // OPTIONS: 'dark', 'ultra-dark', 'bright'
+    //   vibrancy: 'dark'
     // }
   },
 
   plugins: [
+    'hyper-search',
     'hyper-statusline',
     'hyper-tabs-enhanced',
-    'hyper-search',
     'hypercwd',
-    // "hypersixteen",
+    "hypersixteen"
     // "hyper-material-theme",
     // "nord-hyper",
+    // "hyperterm-tomorrow-night",
     // "hyperterm-hybrid",
-    "hyper-tomorrow-night"
+    // "hyper-iceberg",
   ],
 
   localPlugins: [],
 
-  keymaps: {
-  },
+  keymaps: {},
 };
 
