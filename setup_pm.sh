4787ab4affdb7f3cb808c8285b0011f3967c6964
ichichou <35998815+ichichou@users.noreply.github.com>
2019-08-18 17:49:31 +0900

    npm を削除

--- setup_pm.sh
+++ setup_pm.sh
@@ -9,15 +9,9 @@ fisher add jethrokuan/fzf
 fisher add jethrokuan/z
 fisher add rafaelrinaldi/pure
 
-gem update --system
-gem install asciidoctor
-gem install asciidoctor-pdf --pre
-gem install asciidoctor-pdf-cjk
-gem install asciidoctor-diagram
-
-npm update -g npm
-npm install -g gitbook-cli
-npm install -g mermaid
-npm install -g mermaid.cli
-npm install -g instant-markdown-d
+# gem update --system
+# gem install asciidoctor
+# gem install asciidoctor-pdf --pre
+# gem install asciidoctor-pdf-cjk
+# gem install asciidoctor-diagram
 
