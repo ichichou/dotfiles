759e2f1f168d322195556e9a54b579f8fbeb7554
ichichou <35998815+ichichou@users.noreply.github.com>
2019-09-08 18:24:44 +0900

    fishfile を使うよう変更

--- setup_pm.sh
+++ setup_pm.sh
@@ -4,10 +4,9 @@ set -eu
 
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 fisher self-update
-fisher add 0rax/fish-bd
-fisher add jethrokuan/fzf
-fisher add jethrokuan/z
-fisher add rafaelrinaldi/pure
+fisehr
+
+set -U fish_user_paths /usr/local/opt/ruby/bin $fish_user_paths
 
 # gem update --system
 # gem install asciidoctor
