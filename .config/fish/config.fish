e2d42b122b8abdfc012b0758d781e16cc508428d
ichichou <35998815+ichichou@users.noreply.github.com>
2019-12-12 11:13:30 +0900

    インデントを調整

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,9 +5,9 @@ alias reload 'exec fish'
 alias symln 'ln -snfv'
 
 if test -e "/usr/local/bin/rmtrash"
-  alias rm 'rmtrash'
+    alias rm 'rmtrash'
 else
-  alias rm 'rm -iv'
+    alias rm 'rm -iv'
 end
 
 alias b 'brew'
@@ -22,26 +22,26 @@ alias t 'tmux'
 alias vi 'vim'
 
 if test -e "/usr/local/bin/exa"
-  alias ls 'exa -a'
-  alias ll 'exa -1a'
-  alias la 'exa -al --git'
+    alias ls 'exa -a'
+    alias ll 'exa -1a'
+    alias la 'exa -al --git'
 else
-  alias ls 'ls -aG'
-  alias ll 'ls -1aG'
-  alias la 'ls -alG'
+    alias ls 'ls -aG'
+    alias ll 'ls -1aG'
+    alias la 'ls -alG'
 end
 
 ## functions
 functions --copy cd standard_cd
 function cd
-  standard_cd $argv; and la
+    standard_cd $argv; and la
 end
 
 function mozjall
-  mkdir -p mozjpeg
-  for i in *.jpg
-    mozcjpeg -optimize $i > mozjpeg/$i
-  end
+    mkdir -p mozjpeg
+    for i in *.jpg
+        mozcjpeg -optimize $i > mozjpeg/$i
+    end
 end
 
 ## fish
