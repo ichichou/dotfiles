6354b8f6db9862277c49f8896b3504193b5eb858
ichichou <35998815+ichichou@users.noreply.github.com>
2020-01-27 17:08:40 +0900

    fzf の設定を変更

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -50,3 +50,5 @@ set fish_greeting
 
 ## fzf
 set -U FZF_LEGACY_KEYBINDINGS 0
+set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
+set -x FZF_FIND_FILE_COMMAND $FZF_DEFAULT_COMMAND
