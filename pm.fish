a2a9b0972692c8da1ac7f97ce0b1e5572939d20e
ichichou <35998815+ichichou@users.noreply.github.com>
2020-02-29 01:01:04 +0900

    インストール順序を変更

--- pm.fish
+++ pm.fish
@@ -1,7 +1,5 @@
 #!/usr/local/bin/fish
 
-set -eu
-
 if test ! -f ~/.config/fish/functions/fisher.fish
     curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 end
@@ -11,9 +9,9 @@ fisehr
 
 npm -g install npm
 npm -g install eslint
+npm -g install prettier
 npm -g install eslint-plugin-prettier
 npm -g install htmlhint
-npm -g install prettier
 
 gem update --system
 gem install asciidoctor
