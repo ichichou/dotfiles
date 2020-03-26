bb23c26a2400d43023805619894bdc72b306e421
ichichou <35998815+ichichou@users.noreply.github.com>
2020-03-26 15:05:13 +0900

    エイリアスを更新

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,48 +1,48 @@
-## aliases
-alias cp 'cp -iv'
-alias mv 'mv -iv'
-alias reload 'exec fish'
-alias symln 'ln -snfv'
+## Aliases
+alias cp "cp -iv"
+alias mv "mv -iv"
+alias reload "exec fish"
+alias symln "ln -snfv"
 
-alias b 'brew'
-alias bc 'brew cask'
-alias g 'git'
-alias gu 'gitup'
-alias j 'z'
-alias jl 'jupyter lab'
-alias mvi 'mvim --remote-tab-silent'
-alias t 'tmux'
-alias vi 'vim'
-
-if test -e '/usr/local/bin/rmtrash'
-    alias rm 'rmtrash'
+if test -e "/usr/local/bin/rmtrash"
+    alias rm "rmtrash"
 else
-    alias rm 'rm -iv'
+    alias rm "rm -iv"
 end
 
-if test -e '/usr/local/bin/exa'
-    alias ls 'exa -a'
-    alias ll 'exa -1a'
-    alias la 'exa -al --git'
+if test -e "/usr/local/bin/exa"
+    alias ls "exa -a"
+    alias ll "exa -1a"
+    alias la "exa -al --git"
 else
-    alias ls 'ls -aG'
-    alias ll 'ls -1aG'
-    alias la 'ls -alG'
+    alias ls "ls -AG"
+    alias ll "ls -1AG"
+    alias la "ls -AlG"
 end
 
-## binds
+alias b "brew"
+alias bc "brew cask"
+alias g "git"
+alias gu "gitup"
+alias j "z"
+alias jl "jupyter lab"
+alias mvi "mvim --remote-silent"
+alias t "tmux"
+alias vi "vim"
+
+## Keybinds
 bind -e \cl
 bind -e \cj
 bind -e \cf
 
-## functions
-### auto ls
+## Functions
+### Auto ls
 functions --copy cd standard_cd
 function cd
     standard_cd $argv; and ls
 end
 
-### asciidoctor
+### Asciidoctor
 function adoc
     asciidoctor -r asciidoctor-diagram $argv
 end
@@ -51,7 +51,7 @@ function adocpdf
     asciidoctor-pdf -r asciidoctor-pdf-cjk -r asciidoctor-diagram $argv
 end
 
-## fish
+## Fish
 set fish_greeting
 
 ## fzf
@@ -59,6 +59,6 @@ set -x FZF_LEGACY_KEYBINDINGS 0
 set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
 set -x FZF_FIND_FILE_COMMAND $FZF_DEFAULT_COMMAND
 
-## ruby
+## Ruby
 set -x fish_user_paths /usr/local/opt/ruby/bin $fish_user_paths
 set -x fish_user_paths $fish_user_paths /usr/local/lib/ruby/gems/2.7.0/bin
