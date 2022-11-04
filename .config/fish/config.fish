db1ff9b3afb0569be2210fa73909c7ee8f4c4067
ichichou <35998815+ichichou@users.noreply.github.com>
2022-11-04 22:12:32 +0900

    Zk の環境変数を追加

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -123,5 +123,8 @@ set -x FZF_LEGACY_KEYBINDINGS 0
 set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
 set -x FZF_FIND_FILE_COMMAND $FZF_DEFAULT_COMMAND
 
+## Zk
+set -x ZK_NOTEBOOK_DIR ~/repos/zk
+
 ## Greeting
 set fish_greeting
