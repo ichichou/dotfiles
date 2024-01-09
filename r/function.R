0e1ca2fbf526df184d81a095b08028e76c807399
ichichou <35998815+ichichou@users.noreply.github.com>
2024-01-09 13:25:33 +0900

    Update & rename star-up file for R

---
+++ r/function.R
@@ -0,0 +1,19 @@
+myenv <- new.env()
+
+# Clear console messages
+myenv$clear_console <- function() cat("\014")
+
+# Clear plots
+myenv$clear_plots <- function() if (dev.cur() > 1) dev.off()
+
+# Clear global environment
+myenv$clear_globalenv <- function() rm(list = ls(envir = globalenv()), envir = globalenv())
+
+# Clear all
+myenv$clear_all <- function() {
+  clear_console()
+  clear_plots()
+  clear_globalenv()
+}
+
+attach(myenv)
