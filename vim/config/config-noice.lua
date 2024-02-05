d9134b0f256744f628d3c9208a3ac13471ef6b1c
ichichou <35998815+ichichou@users.noreply.github.com>
2024-02-05 20:50:05 +0900

    Update Noice.vim config

--- vim/config/config-noice.lua
+++ vim/config/config-noice.lua
@@ -1,18 +1,38 @@
 require("noice").setup({
   cmdline = {
-    view = "cmdline",
+    -- view = "cmdline",
   },
+
   messages = {
     view = "mini",
     view_error = "mini",
     view_warn = "mini",
   },
-  -- views = {
-  --   mini = {
-  --     timeout = 5000,
-  --     position = {
-  --       row = 0,
-  --     },
-  --   },
-  -- },
+
+  views = {
+
+    cmdline_popup = {
+      border = {
+        style = "none",
+        padding = { 1, 2 },
+      },
+      win_options = {
+        winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+      },
+    },
+
+    -- cmdline_popup = {
+    --   border = {
+    --     style = "single"
+    --   },
+    -- },
+
+    -- mini = {
+    --   timeout = 5000,
+    --   position = {
+    --     row = 0,
+    --   },
+    -- },
+
+  },
 })
