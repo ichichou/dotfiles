accd2b6c552dd27bfe5ca95be6937a6bb42aaa9f
ichichou <35998815+ichichou@users.noreply.github.com>
2024-02-06 02:27:53 +0900

    Update Vim mapping for git plugins

--- vim/config/config-gitsigns.lua
+++ vim/config/config-gitsigns.lua
@@ -1,5 +1,34 @@
 require('gitsigns').setup {
+
   signcolumn = true,
   numhl      = false,
   linehl     = false,
+
+  -- Keymaps
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', '<Leader>gj', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    map('n', '<Leader>gk', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    -- Actions
+    map('n', '<Leader>gd', gs.preview_hunk)
+
+  end
 }
--- vim/config/config-signify.vim
+++ vim/config/config-signify.vim
@@ -1,12 +1,16 @@
 UsePlugin 'vim-signify'
 
-nmap <Leader>gj <Plug>(signify-next-hunk)
-nmap <Leader>gk <Plug>(signify-prev-hunk)
-nmap <Leader>gJ 9999<Leader>gj
-nmap <Leader>gK 9999<Leader>gk
+nnoremap <Leader>g <Nop>
+nnoremap [signify] <Nop>
+nmap <Leader>g [signify]
 
-nmap <Leader>gd <Cmd>SignifyHunkDiff<CR>
-nmap <Leader>gu <Cmd>SignifyHunkUndo<CR>
+nmap [signify]j <Plug>(signify-next-hunk)
+nmap [signify]k <Plug>(signify-prev-hunk)
+nmap [signify]J 9999<Leader>gj
+nmap [signify]K 9999<Leader>gk
+
+nmap [signify]d <Cmd>SignifyHunkDiff<CR>
+nmap [signify]u <Cmd>SignifyHunkUndo<CR>
 
 function! s:ShowCurrentHunk() abort
   let h = sy#util#get_hunk_stats()
