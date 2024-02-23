cd41a88d1b09f9b84f3e66d98522f205b8f77e42
ichichou <35998815+ichichou@users.noreply.github.com>
2024-02-23 09:56:09 +0900

    Update setup*.sh

--- setup_localized.sh
+++ setup_localized.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 set -eu
 
-sudo sh -c "rm -v ${HOME}/Applications/.localized"
-sudo sh -c "rm -v ${HOME}/Desktop/.localized"
-sudo sh -c "rm -v ${HOME}/Documents/.localized"
-sudo sh -c "rm -v ${HOME}/Downloads/.localized"
-sudo sh -c "rm -v ${HOME}/Library/.localized"
-sudo sh -c "rm -v ${HOME}/Movies/.localized"
-sudo sh -c "rm -v ${HOME}/Music/.localized"
-sudo sh -c "rm -v ${HOME}/Pictures/.localized"
-sudo sh -c "rm -v ${HOME}/Public/.localized"
+file=".localized"
+
+# Home -------------------------------------------
+
+sudo sh -c "rm -fv ${HOME}/Applications/${file}"
+sudo sh -c "rm -fv ${HOME}/Desktop/${file}"
+sudo sh -c "rm -fv ${HOME}/Documents/${file}"
+sudo sh -c "rm -fv ${HOME}/Downloads/${file}"
+sudo sh -c "rm -fv ${HOME}/Library/${file}"
+sudo sh -c "rm -fv ${HOME}/Movies/${file}"
+sudo sh -c "rm -fv ${HOME}/Music/${file}"
+sudo sh -c "rm -fv ${HOME}/Pictures/${file}"
+sudo sh -c "rm -fv ${HOME}/Public/${file}"
+
+# Root -------------------------------------------
+
+sudo sh -c "rm -fv /Applications/${file}"
+sudo sh -c "rm -fv /Library/${file}"
+sudo sh -c "rm -fv /Users/${file}"
+
+# Does not work?
+sudo sh -c "rm -fv /System/${file}"
