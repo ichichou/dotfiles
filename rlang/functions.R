ea23609035782b85487d71fe9b84e83669c54fc6
ichichou <35998815+ichichou@users.noreply.github.com>
2025-01-06 21:50:47 +0900

    Update R functions

--- rlang/functions.R
+++ rlang/functions.R
@@ -1,13 +1,22 @@
 myenv <- new.env()
 
 # Clear console messages
-myenv$clear_console <- function() cat("\014")
+myenv$clear_console <- function() {
+  cat("\014")
+}
 
 # Clear plots
-myenv$clear_plots <- function() if (dev.cur() > 1) dev.off()
+myenv$clear_plots <- function() {
+  if (dev.cur() > 1) dev.off()
+}
 
 # Clear global environment
-myenv$clear_globalenv <- function() rm(list = ls(envir = globalenv()), envir = globalenv())
+myenv$clear_globalenv <- function() {
+  rm(
+    list  = ls(envir = globalenv()),
+    envir = globalenv()
+  )
+}
 
 # Clear all
 myenv$clear_all <- function() {
