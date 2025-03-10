ad667956ae56d9665a0dde383b7c4d17d6f3b7f3
ichichou <35998815+ichichou@users.noreply.github.com>
2025-03-11 03:52:45 +0900

    Fix comments

--- setup_localized.sh
+++ setup_localized.sh
@@ -3,8 +3,6 @@ set -eu
 
 file=".localized"
 
-# Home -------------------------------------------
-
 sudo sh -c "rm -fv ${HOME}/Applications/${file}"
 sudo sh -c "rm -fv ${HOME}/Desktop/${file}"
 sudo sh -c "rm -fv ${HOME}/Documents/${file}"
@@ -15,11 +13,7 @@ sudo sh -c "rm -fv ${HOME}/Music/${file}"
 sudo sh -c "rm -fv ${HOME}/Pictures/${file}"
 sudo sh -c "rm -fv ${HOME}/Public/${file}"
 
-# Root -------------------------------------------
-
 sudo sh -c "rm -fv /Applications/${file}"
 sudo sh -c "rm -fv /Library/${file}"
+# sudo sh -c "rm -fv /System/${file}"  # Does not work
 sudo sh -c "rm -fv /Users/${file}"
-
-# # Does not work:
-# sudo sh -c "rm -fv /System/${file}"
