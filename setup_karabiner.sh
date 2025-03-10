e7a15c8ec1ee2c2b52992cc94be60345f10452af
ichichou <35998815+ichichou@users.noreply.github.com>
2025-03-10 18:33:31 +0900

    Update shell script for Karabiner

--- setup_karabiner.sh
+++ setup_karabiner.sh
@@ -7,16 +7,8 @@ fi
 
 conf_kb="${HOME}/.config/karabier/assets/complex_modifications"
 dot_kb="${HOME}/dotfiles/karabiner"
-input="${HOME}/repos/kana-input"
 
-# dotfiles ---------------------------------------
-
-cd $dot_kb
+cd "$dot_kb"
 for file in *.yml; do
-  yq eval -o=json ${dot_kb}/${file} > ${conf_kb}/${file}
+  yq eval -o=json "$dot_kb"/"$file" > "$conf_kb"/"$file"
 done
-
-# kana-input -------------------------------------
-
-yq eval -o=json ${input}/buna.yml > ${conf_kb}/buna.yml
-yq eval -o=json ${input}/shingeta.yml > ${conf_kb}/shingeta.yml
