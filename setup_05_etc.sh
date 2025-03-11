e9d51071ce977df37f528058178b44b40b6f99e7
ichichou <35998815+ichichou@users.noreply.github.com>
2025-03-12 00:30:13 +0900

    Update shell scripts

--- setup_05_etc.sh
+++ setup_05_etc.sh
@@ -12,9 +12,14 @@ ln -snfv "$dotfiles"/command/ "$HOME"/command
 
 # macSKK -----------------------------------------
 
+if [[ ! -e "/opt/homebrew/bin/gh" ]]; then
+  echo "ERROR: gh command does not exists."
+  exit 1
+fi
+
 cd "$repos"
 gh repo clone skk-dev/dict
 
 cd "$macskk_dir"
-./cp_dicts.sh    # dict
-./cp_configs.sh  # skk-jisyo.utf8, kana-rule.conf
+./cp_dicts.sh    # Copy dictionary files
+./cp_configs.sh  # Copy 'skk-jisyo.utf8' and 'kana-rule.conf'
