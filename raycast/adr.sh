927b74afe9dc678c18ead760cd3f0a2c7a21e2f4
ichichou <35998815+ichichou@users.noreply.github.com>
2025-10-29 16:15:04 +0900

    Add Raycast command

---
+++ raycast/adr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eu
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title ADR: Any Decision Records
+# @raycast.mode silent
+# @raycast.currentDirectoryPath ~/Library/CloudStorage/Box-Box/Notes
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+open -a "bike" adr.bike
