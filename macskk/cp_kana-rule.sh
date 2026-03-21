41795692458415861c4f33833b6209a9451830d9
ichichou <35998815+ichichou@users.noreply.github.com>
2026-03-21 23:17:02 +0900

    Update script

--- macskk/cp_kana-rule.sh
+++ macskk/cp_kana-rule.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eu
 
-source_file="${HOME}/dotfiles/macskk/kana-rule.conf"
-target_file="${HOME}/Library/Containers/net.mtgto.inputmethod.macSKK/Data/Documents/Settings/kana-rule.conf"
+source_file="${HOME}/dotfiles/macskk/kana-rule_ichichou.conf"
+target_file="${HOME}/Library/Containers/net.mtgto.inputmethod.macSKK/Data/Documents/Settings/kana-rule_ichichou.conf"
 
 if [[ ! -e "$source_file" ]]; then
-  echo "ERROR: kana-rule.conf does not exists."
+  echo "ERROR: kana-rule_ichichou.conf does not exists."
   exit 1
 fi
 
